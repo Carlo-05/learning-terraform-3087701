402badc0d46894a61d01c18e42029ff520066ccf
Carlo Ralphael Bravo <104559034+Carlo-05@users.noreply.github.com>
2023-06-16 13:01:17 +0800

    edit vpc id

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -45,7 +46,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog"
 
-  vpc_id              = module.vpc.public_subnets[0]
+  vpc_id              = module.vpc.vpc_id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
